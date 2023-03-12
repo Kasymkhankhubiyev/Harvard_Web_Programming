e9f8fd010b7583745613908f0b4f463397a43265
Kaseke <kasymkhankhubievnis@gmail.com>
2023-03-12 14:57:18 +0700

    added a new app

--- harvard_web/hello/urls.py
+++ harvard_web/hello/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<str:name>", views.greeting, name='greeting'),
     path("kasym", views.kasym, name="kasym"),
     path("azamat", views.azamat, name="azamat"),
 ]
--- harvard_web/hello/views.py
+++ harvard_web/hello/views.py
@@ -4,10 +4,21 @@ from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello, world!")
+    # return HttpResponse("Hello, world!")
+    return render(request, 'hello/index.html')
 
 def kasym(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello, Kasym!")
 
 def azamat(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello, Azamat!")
+
+def greeting(request: HttpRequest, name: str) -> HttpResponse:
+    """первый аргумент - это запрос,
+    второй аргумент - это шаблон 
+    третий аргумент - опциональный, содержит контекст, т.е. доп данные
+    """
+    # return HttpResponse(f'Hello, {name.capitalize()}!')
+    return render(request, 'hello/greeting.html', {
+        "name": name.capitalize(),
+    })
