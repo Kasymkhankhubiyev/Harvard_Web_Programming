d0e8b768f158cc60d3382c24b78267ca39b84b12
Kaseke <kasymkhankhubievnis@gmail.com>
2023-03-14 22:57:15 +0700

    created a super user and registered models

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from django.http import HttpRequest, HttpResponse
+from .models import Flight
+
 # Create your views here.
+def index(request) -> HttpResponse:
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all()
+    })
