895f3f1a0e27bfc21ff0e018fcd4798ccc677600
Kaseke <kasymkhankhubievnis@gmail.com>
2023-03-22 17:17:34 +0700

    commit changes

--- SinglePageApp/pages/views.py
+++ SinglePageApp/pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse
 
 posts = ['post1', 'post2', 'post3', 'post4', 'post5', 'post6', 
          'post7', 'post8', 'post9', 'post10', 'post11', 'post12', 
@@ -12,6 +12,21 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'pages/index.html')
 
 def news(request: HttpRequest) -> HttpResponse:
-    return render(request, 'pages/news.html', {
-        'posts': posts[:7]
+    return render(request, 'pages/news.html')
+
+def posts(request: HttpRequest) -> HttpResponse:
+    # Get start and end points
+    start = int(request.GET.get('start') or 0)
+    end = int(request.GET.get('end') or (start + 7))
+
+    # Generate a list of posts:
+    data = []
+    for i in range(start, end + 1):
+        data.append(f'Post #{i}')
+
+    # return render(request, 'pages/news.html', {
+    #     'posts': data
+    # })
+    return JsonResponse({
+        "posts": data
     })
