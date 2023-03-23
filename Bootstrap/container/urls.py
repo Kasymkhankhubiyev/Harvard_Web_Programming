fe6259479d9619f904ab3116890f2d637a7b0bc6
Kaseke <kasymkhankhubievnis@gmail.com>
2023-03-23 18:04:45 +0700

    added some pages

---
+++ Bootstrap/container/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("container1", views.container1, name='container1'),
+    path("container2", views.container2, name='container2'),
+    path("grid", views.grid, name='grid'),
+    path("typography", views.typography, name='typography')
+]
--- Bootstrap/container/views.py
+++ Bootstrap/container/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from django.http import HttpRequest, HttpResponse
+
 # Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, 'container/index.html')
+
+def container1(request: HttpRequest) -> HttpResponse:
+    return render(request, 'container/container1.html')
+
+def container2(request: HttpRequest) -> HttpResponse:
+    return render(request, 'container/container2.html')
+
+def grid(request: HttpRequest) -> HttpResponse:
+    return render(request, 'container/grid.html')
+
+def typography(request: HttpRequest) -> HttpResponse:
+    return render(request, 'container/typography.html')
